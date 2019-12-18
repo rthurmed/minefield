cbee80388f11a808efec86eca813714bacceb9f1
Arthur <rthurmedeiros@gmail.com>
2019-12-17 22:46:18 -0300

    Changed background color

--- main.lua
+++ main.lua
@@ -16,6 +16,8 @@ function love.load()
   windowHeight = spriteHeight * howManyLines + 1
   windowWidth = spriteWidth * howManyColumns + 1
   love.window.setMode(windowWidth, windowHeight)
+  -- Sets background color to green
+  love.graphics.setBackgroundColor(0/255, 194/255, 19/255)
 
   -- Load sprites
   grass = love.graphics.newImage("assets/grass.png")
